c05b2320095f2253d8d6c3e142e8125626fdb94f
guidobtg97 <97922503+guidobtg97@users.noreply.github.com>
2022-08-22 21:21:54 -0300

    cambios

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <img src="logo.png" alt="logo"></img>
+                    <li>
+                        <a href="https://google.com.">Tienda</a>
+                    </li>
+                    <li>
+                        <a href="https://google.com.">Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="https://google.com.">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
