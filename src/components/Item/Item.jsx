fd5000e926754d492e62b96f477753f90547e4c4
guidobtg97 <guidobtg97@gmail.com>
2022-10-12 01:32:40 -0300

    css

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import '../ItemDetail/ItemDetail.css'
+import '../Item/Item.css';
 
 const Item = (items) => {
   return (
     <Link to={`/detail/${items.id}`}>
         
       <div key={items.id} className='item-detail-padre'>
-        
-        <div>
+        <div className='products-container'>
+        <div className='item-img-container' id='img-container'>
             <img id='prod-img' src={items.img} alt=''/>
         </div>
 
@@ -16,10 +16,10 @@ const Item = (items) => {
             <h4 id="caja-1">{items.title}</h4>
             <h5 id="caja-2">{items.price}</h5>
             <Link to={`/detail/${items.id}`}>
-              <button>Ver detalles</button>
+              <button id='item-button'>Ver detalles</button>
             </Link>
         </div>
-
+        </div>
       </div>
 
     </Link>
